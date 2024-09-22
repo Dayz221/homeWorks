e0ea25f2e51fbbd605c50c50caec77126da455e9
Dayz221 <rutuchenal37@gmail.com>
2024-09-23 00:03:10 +0300

    sd

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
+import { LoginButton } from '@telegram-auth/react'
 import '../styles/login.css'
 
 export default () => {
     const [userPhotoUrl, setUserPhotoUrl] = useState("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_YBNzNGxrDL6LCMEtmMsJRK08flZj0JLOVA&s")
-    
-    function onTelegramAuth(user) {
-        alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
-    }
 
     return (
         <div className="container">
-            
+            <h2 className="title">Войди через телеграмм</h2>
+            <LoginButton
+                botUsername="myHomeworkWithSiteBot"
+                onAuthCallback={(data) => {
+                    console.log(data)
+                }}
+                buttonSize="large"
+                cornerRadius={20}
+                showAvatar={true}
+                lang="ru"
+            />
         </div>
     )
 }
