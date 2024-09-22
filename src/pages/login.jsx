a86bdc7d11debf621aae4c004c09613159e6d537
Dayz221 <rutuchenal37@gmail.com>
2024-09-22 23:11:51 +0300

    ss

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
 import '../styles/login.css'
 
 export default () => {
+    const [userPhotoUrl, setUserPhotoUrl] = useState("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_YBNzNGxrDL6LCMEtmMsJRK08flZj0JLOVA&s")
+    
+    function onTelegramAuth(user) {
+        alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+    }
+
     return (
         <div className="container">
-
+            
         </div>
     )
 }
