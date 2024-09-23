4127417565c21a1f1b8f66c2febdf8dbe980bb1f
Dayz221 <rutuchenal37@gmail.com>
2024-09-23 08:13:12 +0300

    j

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,25 +1,13 @@
-import { useRef, useState } from 'react'
 import { LoginButton } from '@telegram-auth/react'
 import '../styles/login.css'
 
 export default () => {
-    const [userPhotoUrl, setUserPhotoUrl] = useState("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_YBNzNGxrDL6LCMEtmMsJRK08flZj0JLOVA&s")
-
-    const btnRef = useRef(null)
-
-    const handleObjectClick = () => {
-        if (btnRef.current) {
-            btnRef.current.click(); // Вызов клика по кнопке
-        }
-    }
-
     return (
-        <div style={{padding: "0 10px"}}>
+        <div style={{padding: "0 15px"}}>
             <div className="container">
                 <h2 className="title">Войди через телеграмм</h2>
-                
+
                 <LoginButton
-                    className = "telegram-widget"
                     botUsername="myHomeworkWithSiteBot"
                     onAuthCallback={(data) => {
                         console.log(data)
