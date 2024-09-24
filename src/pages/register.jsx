6ae0dc4b3c402c1d3f63cdfa845973b7952b55ee
Dayz221 <rutuchenal37@gmail.com>
2024-09-24 23:39:40 +0300

    sd

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -17,7 +17,8 @@ export default () => {
             {/* {isLoading ? <Loader/> : null} */}
             <div style={{padding: "0 15px"}}>
                 <div className="register_container">
-                    <h2 className="title">Регистрация</h2>
+                    <h2 className="register_title">Регистрация</h2>
+                    
                     <div className="user_info__container">
                         <div className="image_container">
                             <img src={user.photo_url} />
@@ -26,6 +27,17 @@ export default () => {
                             <div className="full_name">{user.last_name + " " + user.first_name}</div>
                             <div className="user_username">{"@"+user.username}</div>
                         </div>
+                    </div>
+
+                    <div className="group_select__container">
+                        <input className="register_input" type="text" />
+                        <input className="register_input" type="password" />
+                        
+                        <button className="send_button">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14 6L20 12M20 12L14 18M20 12H5" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </button>
                     </div>
                 </div>
             </div>
